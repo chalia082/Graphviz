f729d92773282e60408cad4775cf1893b36437a3
chalia082 <59096707+chalia082@users.noreply.github.com>
2023-09-08 16:46:42 -0500

    printed files

--- app.py
+++ app.py
@@ -11,3 +11,11 @@ dot.edge('B', 'L', constraint='false')
 
 print(dot.source)
 dot.render('round-table.gv', view=True)
+
+w = Digraph('wide')
+
+w.edges(('0', str(i)) for i in range(1, 10))
+w.view()
+
+u = w.unflatten(stagger=3)
+u.view()
